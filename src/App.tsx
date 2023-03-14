d08b0a2fa66b6ec37da09b7282be77f77e17cd5c
Carlos Eduardo <dudu.lima.bastos@gmail.com>
2023-03-14 16:36:53 -0300

    configurando theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import {data} from './api/db'
 
 function App() {
-  console.log(data)
 
   return (
     <div className="App">
       <div>
-        {data.map(product => (
-          <div>
-            <p>{product.title}</p>
-            <img src={product.image}/>
-          </div>
-        ))}
       </div>
     </div>
   );
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,13 +1,25 @@
 export const theme = {
   colors: {
+    primaryColor: '#ffc700',
+    secondaryColor: '#10a958',
+    darkColor: '#000000',
+    lightColor: '#fff'
   },
   fonts: {
     family: {
+      default: "'Open Sans', sans-serif",
+      secondaryFont: "'Oswald', sans-serif"
     },
     sizes: {
+      small: '0.875rem',
+      xsmall: '1.25rem',
+      medium: '1.5rem',
+      xmedium: '1.625rem',
+      large: '3rem'
     },
   },
   media: {
+    lteMedium: '(max-width: 767px)',
   },
   opacity: {
   },
