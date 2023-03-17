d549e824e93a48342a6ddfd1aff1e94ff01ad38e
Carlos Eduardo <dudu.lima.bastos@gmail.com>
2023-03-17 14:07:22 -0300

    Criando testes para TextComponent

--- src/components/TextComponent/index.tsx
+++ src/components/TextComponent/index.tsx
@@ -2,8 +2,8 @@ import * as Styled from './styles';
 
 export type TextComponentProps = {
   children: React.ReactNode;
-  hasBackground: boolean,
-  uppercase: boolean,
+  hasBackground?: boolean,
+  uppercase?: boolean,
 };
 
 export const TextComponent = ({children, hasBackground = false, uppercase = false}:TextComponentProps) => {
