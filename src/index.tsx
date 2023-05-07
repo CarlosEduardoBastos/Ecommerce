9c4fcf49831f22f379986e1296e5ee4b246c4b56
Carlos Eduardo <dudu.lima.bastos@gmail.com>
2023-05-07 13:53:38 -0300

    criado a page home e cocando o estilo global

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import { ProductProvider } from './store/context/ProductProvider';
 import { theme } from './styles/theme';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { GlobalStyles } from './styles/global-styles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <ThemeProvider theme={theme}>
+    <GlobalStyles />
     <ProductProvider>
       <BrowserRouter>
+      <Header />
         <App />
+        <Footer />
       </BrowserRouter>
     </ProductProvider>
   </ThemeProvider>
